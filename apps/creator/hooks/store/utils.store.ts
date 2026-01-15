23a59d3fd857c0c839196786d69fcbce72914d2a
ARIJIT <arijitchhatui.me@gmail.com>
2026-01-15 21:35:22 +0530

    fix:fix: Implement the /studio/paths page (#65)
    Fixes #64
    /cards page on creator app should be refactored
    Fixes #35
    In creator app the /subscriptions and /subscriptions/plan page needs some padding
    Fixes #36

--- apps/creator/hooks/store/utils.store.ts
+++ apps/creator/hooks/store/utils.store.ts
@@ -1,19 +1,24 @@
+import { GetPostsInfoOutput, PostsEntity } from '@workspace/gql/generated/graphql';
 import { BackGroundColors } from '@workspace/ui/lib/enums';
 import { create } from 'zustand';
 
 type UtilsStore = {
-  openAsstPickerModal: boolean;
+  shareModal: GetPostsInfoOutput | null;
   openLogoutModal: boolean;
   bgColor: BackGroundColors;
+  openAsstPickerModal: boolean;
+  setShareModal: (shareModal: null | GetPostsInfoOutput) => void;
   setBgColor: (bgColor: BackGroundColors) => void;
   setOpenLogoutModal: (openLogoutModal: boolean) => void;
   setOpenAsstPickerModal: (openAsstPickerModal: boolean) => void;
 };
 
 export const useUtilsStore = create<UtilsStore>()((set) => ({
-  bgColor: BackGroundColors.DEFAULT,
+  shareModal: null,
   openLogoutModal: false,
   openAsstPickerModal: false,
+  bgColor: BackGroundColors.DEFAULT,
+  setShareModal: (shareModal: GetPostsInfoOutput | null) => set({ shareModal }),
   setBgColor: (bgColor: BackGroundColors) => set({ bgColor }),
   setOpenAsstPickerModal: (openAsstPickerModal: boolean) => set({ openAsstPickerModal }),
   setOpenLogoutModal: (openLogoutModal: boolean) => set({ openLogoutModal })
