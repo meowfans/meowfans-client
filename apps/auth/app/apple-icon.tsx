5cc60074cfa7bd41a28607aa6b0e9c9565ebaf31
Arijit Chhatui <arijitchhatui.me@gmail.com>
2026-02-03 16:24:19 +0530

    fix: change color of auth app icon

--- apps/auth/app/apple-icon.tsx
+++ apps/auth/app/apple-icon.tsx
@@ -23,8 +23,8 @@ export default function Icon() {
       <svg width="180" height="180" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
         <defs>
           <linearGradient id="g" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="0%" stopColor="#6366f1" />
-            <stop offset="100%" stopColor="#d946ef" />
+            <stop offset="0%" stopColor="#63f168ff" />
+            <stop offset="100%" stopColor="#46ef54ff" />
           </linearGradient>
           <filter id="glow" x="-20%" y="-20%" width="140%" height="140%">
             <feGaussianBlur stdDeviation="3" result="blur" />
--- apps/auth/app/opengraph-image.tsx
+++ apps/auth/app/opengraph-image.tsx
@@ -39,8 +39,8 @@ export default function Image() {
         <svg width="128" height="128" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient id="g" x1="0%" y1="0%" x2="100%" y2="100%">
-              <stop offset="0%" stopColor="#6366f1" />
-              <stop offset="100%" stopColor="#d946ef" />
+              <stop offset="0%" stopColor="#63f168ff" />
+              <stop offset="100%" stopColor="#46ef54ff" />
             </linearGradient>
           </defs>
           <path
